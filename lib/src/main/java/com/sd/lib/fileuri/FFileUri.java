692176068134609ed865a3538145e4646b6e4c90
zjun <565061763@qq.com>
2019-07-23 17:21:54 +0800

    first commit

---
+++ lib/src/main/java/com/sd/lib/fileuri/FFileUri.java
@@ -0,0 +1,32 @@
+package com.sd.lib.fileuri;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+
+public class FFileUri
+{
+    private FFileUri()
+    {
+    }
+
+    public static Uri get(Context context, File file)
+    {
+        if (file == null)
+            return null;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        {
+            return FileProvider.getUriForFile(context,
+                    context.getPackageName() + "." + MyFileProvider.class.getSimpleName().toLowerCase(),
+                    file);
+        } else
+        {
+            return Uri.fromFile(file);
+        }
+    }
+}

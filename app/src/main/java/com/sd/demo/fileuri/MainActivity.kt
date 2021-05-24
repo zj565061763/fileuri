6bac9d50e5f873bae6e12bb21bdcb74f4d005b89
zjun <565061763@qq.com>
2021-05-24 15:58:45 +0800

    demo

--- app/src/main/java/com/sd/demo/fileuri/MainActivity.kt
+++ app/src/main/java/com/sd/demo/fileuri/MainActivity.kt
@@ -9,6 +9,7 @@ import com.sd.lib.dldmgr.DownloadInfo
 import com.sd.lib.dldmgr.FDownloadManager
 import com.sd.lib.dldmgr.IDownloadManager
 import com.sd.lib.fileuri.FAlbumImageUri
+import com.sd.lib.fileuri.FAlbumVideoUri
 import com.yanzhenjie.permission.AndPermission
 import com.yanzhenjie.permission.runtime.Permission
 import java.io.File
@@ -16,7 +17,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     val TAG = MainActivity::class.java.simpleName
-    val URL = "http://ilvbfanwe.oss-cn-shanghai.aliyuncs.com///public/attachment/test/noavatar_1.JPG"
+    val URL_IMAGE = "http://ilvbfanwe.oss-cn-shanghai.aliyuncs.com///public/attachment/test/noavatar_1.JPG"
+    val URL_VIDEO = "http://1251020758.vod2.myqcloud.com/8a96e57evodgzp1251020758/602d1d1a5285890800849942893/tRGP04QVdCEA.mp4"
 
     private lateinit var _binding: ActivityMainBinding
 
@@ -24,49 +26,79 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
-
-        FDownloadManager.default.addCallback(_downloadCallback)
     }
 
     override fun onClick(v: View?) {
         when (v) {
-            _binding.btnClick -> {
-                FDownloadManager.default.addTask(URL)
+            _binding.btnClickImage -> {
+                clickImage()
+            }
+            _binding.btnClickVideo -> {
+                clickVideo()
             }
         }
     }
 
     /**
-     * 下载回调
+     * 保存图片
      */
-    private val _downloadCallback = object : IDownloadManager.Callback {
-        override fun onPrepare(info: DownloadInfo) {
-        }
+    private fun clickImage() {
+        val addTask = FDownloadManager.default.addTask(URL_IMAGE)
+        if (addTask) {
+            FDownloadManager.default.addUrlCallback(URL_IMAGE, object : IDownloadManager.Callback {
+                override fun onPrepare(info: DownloadInfo) {
+                }
 
-        override fun onProgress(info: DownloadInfo) {
-        }
+                override fun onProgress(info: DownloadInfo) {
+                }
 
-        override fun onSuccess(info: DownloadInfo, file: File) {
-            AndPermission.with(this@MainActivity).runtime()
-                .permission(Permission.Group.STORAGE)
-                .onGranted {
-                    saveFile(file)
-                }.onDenied {
-                    finish()
-                }.start()
-        }
+                override fun onSuccess(info: DownloadInfo, file: File) {
+                    AndPermission.with(this@MainActivity).runtime()
+                        .permission(Permission.Group.STORAGE)
+                        .onGranted {
+                            val uri = FAlbumImageUri.saveFile(file, applicationContext)
+                            Log.i(TAG, "image saveFile:${uri}")
+                        }.onDenied {
+                            finish()
+                        }.start()
+                }
 
-        override fun onError(info: DownloadInfo) {
+                override fun onError(info: DownloadInfo) {
+                }
+            })
         }
     }
 
-    private fun saveFile(file: File) {
-        val uri = FAlbumImageUri.saveFile(file, this)
-        Log.i(TAG, "uri:${uri}")
+    private fun clickVideo() {
+        val addTask = FDownloadManager.default.addTask(URL_VIDEO)
+        if (addTask) {
+            FDownloadManager.default.addUrlCallback(URL_VIDEO, object : IDownloadManager.Callback {
+                override fun onPrepare(info: DownloadInfo) {
+                }
+
+                override fun onProgress(info: DownloadInfo) {
+                }
+
+                override fun onSuccess(info: DownloadInfo, file: File) {
+                    AndPermission.with(this@MainActivity).runtime()
+                        .permission(Permission.Group.STORAGE)
+                        .onGranted {
+                            val uri = FAlbumVideoUri.saveFile(file, applicationContext)
+                            Log.i(TAG, "video saveFile:${uri}")
+                        }.onDenied {
+                            finish()
+                        }.start()
+                }
+
+                override fun onError(info: DownloadInfo) {
+                }
+            })
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        FDownloadManager.default.removeCallback(_downloadCallback)
+        FDownloadManager.default.cancelTask(URL_IMAGE)
+        FDownloadManager.default.cancelTask(URL_VIDEO)
     }
 }

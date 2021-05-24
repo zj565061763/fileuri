50bac196fcdfe21c9943c3086e3dc447eaf30538
zjun <565061763@qq.com>
2021-05-24 16:09:07 +0800

    FFileUri

--- lib/src/main/java/com/sd/lib/fileuri/FFileUri.kt
+++ lib/src/main/java/com/sd/lib/fileuri/FFileUri.kt
@@ -1,32 +1,22 @@
-package com.sd.lib.fileuri;
+package com.sd.lib.fileuri
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Build;
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import androidx.core.content.FileProvider
+import java.io.File
 
-import androidx.core.content.FileProvider;
-
-import java.io.File;
-
-public class FFileUri
-{
-    private FFileUri()
-    {
-    }
-
-    public static Uri get(Context context, File file)
-    {
-        if (file == null)
-            return null;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
-        {
-            return FileProvider.getUriForFile(context,
-                    context.getPackageName() + "." + FFileProvider.class.getSimpleName().toLowerCase(),
-                    file);
-        } else
-        {
-            return Uri.fromFile(file);
+object FFileUri {
+    @JvmStatic
+    fun getUri(file: File, context: Context): Uri {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            FileProvider.getUriForFile(
+                context,
+                context.packageName + "." + FFileProvider::class.java.simpleName.toLowerCase(),
+                file
+            )
+        } else {
+            Uri.fromFile(file)
         }
     }
 }

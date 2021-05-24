79cf16001c9104429fb052ad82c62cce56f49564
zjun <565061763@qq.com>
2021-05-24 10:49:35 +0800

    FAlbumImageUri

--- app/src/main/java/com/sd/demo/fileuri/MainActivity.kt
+++ app/src/main/java/com/sd/demo/fileuri/MainActivity.kt
@@ -1,14 +1,72 @@
 package com.sd.demo.fileuri
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.sd.demo.fileuri.databinding.ActivityMainBinding
+import com.sd.lib.dldmgr.DownloadInfo
+import com.sd.lib.dldmgr.DownloadManagerConfig
+import com.sd.lib.dldmgr.FDownloadManager
+import com.sd.lib.dldmgr.IDownloadManager
+import com.sd.lib.fileuri.FAlbumImageUri
+import java.io.File
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     val TAG = MainActivity::class.java.simpleName
     val URL = "http://ilvbfanwe.oss-cn-shanghai.aliyuncs.com///public/attachment/test/noavatar_1.JPG"
 
+    private lateinit var _binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        DownloadManagerConfig.init(DownloadManagerConfig.Builder().build(this))
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        FDownloadManager.default.addCallback(_downloadCallback)
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            _binding.btnClick -> {
+                FDownloadManager.default.addTask(URL)
+            }
+        }
+    }
+
+    /**
+     * 下载回调
+     */
+    private val _downloadCallback = object : IDownloadManager.Callback {
+        override fun onPrepare(info: DownloadInfo) {
+        }
+
+        override fun onProgress(info: DownloadInfo) {
+        }
+
+        override fun onSuccess(info: DownloadInfo, file: File) {
+            val uuid = UUID.randomUUID().toString()
+            val imageUri = FAlbumImageUri.Builder().apply {
+                this.displayName = uuid + file.name
+            }.build(this@MainActivity)
+
+            imageUri.openOutputStream()?.use { outputStream ->
+                file.inputStream().use { inputStream ->
+                    val copyTo = inputStream.copyTo(outputStream)
+                    Log.i(TAG, "copyTo:${copyTo}")
+                }
+            }
+        }
+
+        override fun onError(info: DownloadInfo) {
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        FDownloadManager.default.removeCallback(_downloadCallback)
     }
 }
--- lib/src/main/java/com/sd/lib/fileuri/FAlbumImageUri.kt
+++ lib/src/main/java/com/sd/lib/fileuri/FAlbumImageUri.kt
@@ -3,11 +3,11 @@ package com.sd.lib.fileuri
 import android.content.ContentValues
 import android.content.Context
 import android.net.Uri
+import android.os.Environment
 import android.provider.MediaStore
 import android.webkit.MimeTypeMap
 import java.io.FileNotFoundException
 import java.io.OutputStream
-import java.util.*
 
 
 class FAlbumImageUri {
@@ -17,21 +17,9 @@ class FAlbumImageUri {
     private constructor(builder: Builder, context: Context) {
         this.context = context.applicationContext
         val contentValues = ContentValues().apply {
-            builder.relativePath.let {
-                this.put(MediaStore.Images.ImageColumns.RELATIVE_PATH, it)
-            }
-            builder.displayName.let {
-                this.put(MediaStore.Images.ImageColumns.DISPLAY_NAME, it)
-            }
-            builder.mimeType.let {
-                this.put(MediaStore.Images.ImageColumns.MIME_TYPE, it)
-            }
-            builder.width?.let {
-                this.put(MediaStore.Images.ImageColumns.WIDTH, it)
-            }
-            builder.height?.let {
-                this.put(MediaStore.Images.ImageColumns.HEIGHT, it)
-            }
+            this.put(MediaStore.Images.ImageColumns.RELATIVE_PATH, builder.relativePath)
+            this.put(MediaStore.Images.ImageColumns.DISPLAY_NAME, builder.displayName)
+            this.put(MediaStore.Images.ImageColumns.MIME_TYPE, builder.mimeType)
         }
         this.uri = context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
     }
@@ -50,11 +38,9 @@ class FAlbumImageUri {
     }
 
     class Builder {
-        var relativePath: String = ""
+        var relativePath: String = Environment.DIRECTORY_PICTURES
         var displayName: String = ""
         var mimeType: String = ""
-        var width: Int? = null
-        var height: Int? = null
 
         fun build(context: Context): FAlbumImageUri {
             if (displayName.isEmpty()) {
